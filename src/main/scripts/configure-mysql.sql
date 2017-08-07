a9219789dee68cd34627879b93c67f06c11f4a40
John Thompson <john@springframework.guru>
2017-08-07 13:44:10 -0400

    adding script statements for connecting from any host

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -9,6 +9,8 @@ CREATE DATABASE sfg_prod;
 #Create database service accounts
 CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
 CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
 
 #Database grants
 GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
@@ -19,3 +21,11 @@ GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
 GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
 GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
 GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
